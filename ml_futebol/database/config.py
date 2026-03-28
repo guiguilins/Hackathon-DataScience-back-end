7e035e1865fb901217e5cb1195e746d64a9affd8
unknown <kkeusoeu@gmail.com>
2026-03-28 00:10:02 -0300

    Criação e configuração do agente

--- ml_futebol/database/config.py
+++ ml_futebol/database/config.py
@@ -9,7 +9,7 @@ class Settings:
     POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
     POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
-    POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
+    POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "55432"))
 
     @property
     def conninfo(self) -> str:
